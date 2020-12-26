b07be8e54dbcdc55c67662ad35b0c5b9bb1d9c71
ssar85 <ssar85@mail.ru>
2020-12-26 17:37:52 +0500

    Задание 4

---
+++ Lesson4-4/Lesson4-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int counter = 0; counter < n; counter++)
+                Console.Write($"{Fib(counter)} ,");
+
+        }
+
+        static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+            {
+                return Fib(n - 2) + Fib(n - 1);
+            }
+
+        }
+    }
+}
